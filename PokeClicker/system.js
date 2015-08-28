741096abce837d61453cfb1cca43086713a1c22a
Isha <I.E.Dijcks@student.tudelft.nl>
2015-08-28 16:41:20 +0200

    Added reset button

--- PokeClicker/system.js
+++ PokeClicker/system.js
@@ -366,6 +366,13 @@ $(document).ready(function(){
 
 	})		
 	
+	$("body").on('click',"#resetButton", function(){
+		var input = prompt("Are you sure you want to delete your savefile?, enter 6 if you are!","9");
+		if (input == 6){
+			localStorage.clear();
+			location.reload();
+		}
+	})
 	// Logs to welcome the player
 	log("Welcome to PokeClicker");
 	log("Click on the pokemon to defeat them!");
