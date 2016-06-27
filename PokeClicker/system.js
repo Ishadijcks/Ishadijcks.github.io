02428bcf9c7b0e4a6bfdd5f40b724ea83e64ae89
Isha Dijcks <i.e.dijcks@student.tudelft.nl>
2016-06-27 15:52:03 +0200

    Did not work... did not work.

--- PokeClicker/system.js
+++ PokeClicker/system.js
@@ -41,24 +41,7 @@ var curEnemy = {
 $(document).ready(function(){
 	//$('#changeLogModal').modal('show');
  
-(function($) {
-  $.fn.nodoubletapzoom = function() {
-      $(this).bind('touchstart', function preventZoom(e) {
-        var t2 = e.timeStamp
-          , t1 = $(this).data('lastTouch') || t2
-          , dt = t2 - t1
-          , fingers = e.originalEvent.touches.length;
-        $(this).data('lastTouch', t2);
-        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
 
-        e.preventDefault(); // double tap - prevent the zoom
-        // also synthesize click events we just swallowed up
-        $(this).trigger('click').trigger('click');
-      });
-  };
-})(jQuery);
-
- 
 	if(localStorage.getItem("player") != null){
 		load();
 		generatePokemon(player.route);
