e6e275383e17b8b9a215e35815741378b626759d
Isha Dijcks <i.e.dijcks@student.tudelft.nl>
2016-06-28 15:02:10 +0200

    Added all town info

--- PokeClicker/towns.js
+++ PokeClicker/towns.js
@@ -15,18 +15,22 @@ var addTown = function(name,gym,image,shop,reqRoutes){
 
 var moveToTown = function(townName){
 	var town = getTown(townName);
-	if( accessToTown(town.reqRoutes)){
-		inProgress = 0;
-		showTown(town);
-	}
-	else {
-		log("You don't have access to "+ townName + " yet.");
-	}
+	canCatch = 0;
+	$("#catchDisplay").html("");	
+	
+		if( accessToTown(town.reqRoutes)){
+			inProgress = 0;
+			showTown(town);
+		}
+		else {
+			log("You don't have access to "+ townName + " yet.");
+		}
+
 }
 
 var accessToTown = function(routeList){
 	for( var i = 0; i<routeList.length; i++){
-		if(player.routeKills[routeList[i]] > player.routeKillsNeeded){
+		if(player.routeKills[routeList[i]] >= player.routeKillsNeeded){
 			return true;
 		}
 	}
@@ -55,6 +59,13 @@ var showTown = function(town){
 
 var loadTowns = function(){
 	addTown("Pewter City", PewterCityGym(), "images/gyms/pewtercity.png", null, [2]);
+	addTown("Cerulean City", CeruleanCityGym(), "images/gyms/ceruleancity.png", null, [4]);
+	addTown("Vermillion City",VermillionCityGym(), "images/gyms/vermillioncity.png", null, [6]);
+	addTown("Celadon City", CeladonCityGym(), "images/gyms/celadoncity.png", null, [7]);
+	addTown("Saffron City", SaffronCityGym(), "images/gyms/saffroncity.png", null, [5]);
+	addTown("Fuchsia City", FuchsiaCityGym(), "images/gyms/pewtercity.png", null, [18,15]);
+	addTown("Cinnabar Island", CinnabarIslandGym(), "images/gyms/cinnabarisland.png", null, [20]);
+	addTown("Viridian City", ViridianCityGym(), "images/gyms/viridiancity.png", null, [1]);
 }
 
 var getTown = function(townName){
