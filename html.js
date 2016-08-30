14b8c6724f2215417eb4300f1eb3bfafc5840839
Isha Dijcks <i.e.dijcks@student.tudelft.nl>
2016-08-30 02:20:59 +0200

    Added tooltips to the items.

--- html.js
+++ html.js
@@ -94,7 +94,7 @@ var updateItems = function(){
 		}
 		if(player.inventoryList[i] != undefined){
 			if(player.inventoryList[i].quantity > 0 || player.inventoryList[i].time > 0){
-				itemHtml += "<th><img class=smallImage src=images/items/"+player.inventoryList[i].id+".png>"+player.inventoryList[i].name + "<div class='noselect useItemButton' id='item"+player.inventoryList[i].id+"' >Use </div>	</th>";
+				itemHtml += "<th><img title='"+player.inventoryList[i].flavorText+"' class='smallImage tooltipItem' src=images/items/"+player.inventoryList[i].id+".png>"+player.inventoryList[i].name + "<div class='noselect useItemButton' id='item"+player.inventoryList[i].id+"' >Use </div>	</th>";
 				itemHtml += "<th>"+player.inventoryList[i].quantity+"</th>";
 				if(player.inventoryList[i].timeLeft == undefined){
 					itemHtml += "<th>0s</th>"
@@ -105,10 +105,17 @@ var updateItems = function(){
 		}
 		itemHtml += "</tr>";
 
+
 	}
+
+
 
 	$("#itemBody").html(itemHtml);
 
+		$(".tooltipItem").tooltipster({
+			position: "left",
+			delay: 1
+	});
 
 }
 
