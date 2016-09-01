b12f3efd560d760e6e71f7d8f144faa605b0c124
Isha Dijcks <i.e.dijcks@student.tudelft.nl>
2016-09-01 19:27:05 +0200

    Updated types.

--- types.js
+++ types.js
@@ -17,9 +17,11 @@ var DARK = 15;
 var STEEL = 16;
 var FAIRY = 17;
 
+
 var typeEffectiveness = [];
 
 var initTypeEffectiveness = function(){
+	typeEffectiveness = [];
 	for( var i = 0; i<17; i++){
 		var row = [];
 		for(var j = 0; j<17; j++){
@@ -29,10 +31,72 @@ var initTypeEffectiveness = function(){
 	}
 
 
-	typeEffectiveness[NORMAL][ROCK] = 0.5;
+	typeEffectiveness[NORMAL][ROCK] = getNotEffective(NORMAL);
 	typeEffectiveness[NORMAL][GHOST] = 0;
-	typeEffectiveness[NORMAL][STEEL] = 0.5;
-	typeEffectiveness[FIRE][FIRE] = 0.5;
+	typeEffectiveness[NORMAL][STEEL] = getNotEffective(NORMAL);
+	typeEffectiveness[FIRE][FIRE] = getNotEffective(FIRE);
+	typeEffectiveness[FIRE][NORMAL] = getNotEffective(FIRE);
+	typeEffectiveness[FIRE][GRASS] = getVeryEffective(FIRE);
+
+
 }
 
+var typeToNumber = function(type){
+	switch(type) {
+		case "normal":
+			return 0;
+		case "fire":
+			return 1;
+		case "water":
+			return 2;
+		case "electric":
+			return 3;
+		case "grass":
+			return 4;
+		case "ice":
+			return 5;
+		case "fighting":
+			return 6;
+		case "poison":
+			return 7;
+		case "ground":
+			return 8;
+		case "flying":
+			return 9;
+		case "psychic":
+			return 10;
+		case "bug":
+			return 11;
+		case "rock":
+			return 12;
+		case "ghost":
+			return 13;
+		case "dragon":
+			return 14;
+		case "dark":
+			return 15;
+		case "steel":
+			return 16;
+		case "fairy":
+			return 17;
+	}
+}
+
+
+var gainShards = function(type, amount){
+	
+	player.typeShards[typeToNumber(type)] += amount;
+}
+
+var getNotEffective = function(type){
+	return Math.min(0.5 + player.notEffectiveTypeBonus[type], 1);
+}
+
+var getNormal = function(type){
+	return Math.min(1 + player.normalEffectiveTypeBonus[type], 1.5);
+}
+
+var getVeryEffective = function(type){
+	return Math.min(1.5 + player.veryEffectiveTypeBonus[type], 2);
+}
 
