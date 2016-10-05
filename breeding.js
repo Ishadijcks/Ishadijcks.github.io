03410cd1f4aa5cfe213239b9e652b968f8fa0b9d
Isha Dijcks <i.e.dijcks@student.tudelft.nl>
2016-10-05 16:27:13 +0200

    Started on breeding.

---
+++ breeding.js
@@ -0,0 +1,19 @@
+var Egg = function(type, difficulty, minAmount, maxAmount, baseReward){
+	var temp = {
+		type:type,
+		steps:steps,
+		progres: 0
+		pokemon:pokemon
+	}
+	return temp;
+}
+
+var possibleEggs = Array.apply(null, Array(17)).map(Number.prototype.valueOf,0);
+
+var initPossibleEggs = function(){
+	var fireEggs = ["Charmander", "Vulpix", "Growlithe", "Ponyta"];
+	var waterEggs = ["Squirtle", "Lapras", "Staryu", "Psyduck"];
+	var grassEggs = ["Bulbasaur", "Oddish", "Paras", "Bellsprout" ]
+	var fightEggs = ["Hitmonlee", "Hitmonchan", "Machop", "Mankey"]
+
+}
