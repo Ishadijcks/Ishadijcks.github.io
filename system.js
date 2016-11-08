c718f538adab8700f2f45ea971227a2f8ff449d1
Isha Dijcks <i.e.dijcks@student.tudelft.nl>
2016-11-08 11:56:06 +0100

    Added level cap to badges again.

--- system.js
+++ system.js
@@ -479,6 +479,7 @@ var experienceToLevel = function(exp,levelType){
 		level = Math.pow(30*exp,0.475)/(6*Math.sqrt(5));
 		break;
 	}
+	level = Math.min(level, (player.gymBadges.length+2)*10);
 	return Math.max(1, Math.min(100,Math.floor(level)));
 }
 
