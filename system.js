17350e8e06272d7216f772a8981490da04d1c748
Aegyo <ae.jessicat@gmail.com>
2016-11-12 20:21:50 +0000

    Finer queue handling

--- system.js
+++ system.js
@@ -344,21 +344,25 @@ $(document).ready(function(){
 			}
 			if(!walking && !safari.isMoving) {
 				if(keyCode == 38 || keyCode == 87){
+					queue = [];
 					walking = true;
 					safari.walking.up = 1;
 					queue.unshift("up");
 					safariMove('up');
 				} else if(keyCode == 39 || keyCode == 68){
+					queue = [];
 					walking = true;
 					safari.walking.right = 1;
 					queue.unshift("right");
 					safariMove('right');
 				} else if(keyCode == 37 || keyCode == 65){
+					queue = [];
 					walking = true;
 					safari.walking.left = 1;
 					queue.unshift("left");
 					safariMove('left');
 				} else if(keyCode == 40 || keyCode == 83){
+					queue = [];
 					walking = true;
 					safari.walking.down = 1;
 					queue.unshift("down");
@@ -390,8 +394,8 @@ $(document).ready(function(){
 			if(keyCode == 38 || keyCode == 87){
 				safari.walking.up = 0;
 				for (var i=0; i<queue.length; i++) {
-					if (queue[i] == "up") {
-						queue.splice(i);
+					if (queue[0] == "up") {
+						queue.shift();
 					}
 				};
 				if (tmp == 1){ walking = false };
@@ -399,8 +403,8 @@ $(document).ready(function(){
 			} else if(keyCode == 39 || keyCode == 68){
 				safari.walking.right = 0;
 				for (var i=0; i<queue.length; i++) {
-					if (queue[i] == "right") {
-						queue.splice(i);
+					if (queue[0] == "right") {
+						queue.shift();
 					}
 				};
 				if (tmp == 1){ walking = false };
@@ -408,8 +412,8 @@ $(document).ready(function(){
 			} else if(keyCode == 37 || keyCode == 65){
 				safari.walking.left = 0;
 				for (var i=0; i<queue.length; i++) {
-					if (queue[i] == "left") {
-						queue.splice(i);
+					if (queue[0] == "left") {
+						queue.shift();
 					}
 				};
 				if (tmp == 1){ walking = false };
@@ -417,8 +421,8 @@ $(document).ready(function(){
 			} else if(keyCode == 40 || keyCode == 83){
 				safari.walking.down = 0;
 				for (var i=0; i<queue.length; i++) {
-					if (queue[i] == "down") {
-						queue.splice(i);
+					if (queue[0] == "down") {
+						queue.shift();
 					}
 				};
 				if (tmp == 1){ walking = false };
@@ -518,7 +522,9 @@ $(document).ready(function(){
 
 var setNextDirection = function(direction) {
 	if(direction != safari.lastDirection){
-		queue.unshift(direction);
+		if (queue[0] != direction) {
+			queue.unshift(direction);
+		};
 		safari.nextDirection = direction;
 		safari.walking[direction] = 1;
 		walking = true;
