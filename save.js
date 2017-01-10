07a8d4c37d13657fd95b520b461792489268b9c8
Jannes Jonkers <jjonkers2@gmail.com>
2017-01-10 14:12:58 +0100

    Added checking download attribute compatibility

--- save.js
+++ save.js
@@ -99,8 +99,9 @@ var load = function(){
 
 var exportSave = function(){
 	var saveData = btoa(JSON.stringify(player));
-	if(typeof FileReader != "undefined") {
+	if(typeof FileReader != "undefined" && typeof document.getElementById("saveAsFile").download != "undefined") {
 		$("#saveAsFile").attr("download", "pokeclicker.sav").attr('href', URL.createObjectURL(new Blob([saveData])));
+		$("#fileUploadButton").css({"visibility": "visible"});
 	}else{
 		$("#saveAsFile").attr("visibility", "hidden");
 	}
