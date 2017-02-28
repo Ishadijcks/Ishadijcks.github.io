b62d9f5b6d8e443698ae7a1e4373d8c7f65b2ba3
Aegyo <ae.jessicat@gmail.com>
2017-02-28 15:49:24 +0000

    Let back button hide modals (for easy closing on mobile)

--- system.js
+++ system.js
@@ -484,6 +484,16 @@ $(document).ready(function(){
 		}
 	});
 
+	$(".modal").on("shown.bs.modal", function()  { // any time a modal is shown
+		var urlReplace = "#" + $(this).attr('id'); // make the hash the id of the modal shown
+		history.pushState(null, null, urlReplace); // push state that hash into the url
+	});
+
+	// If a pushstate has previously happened and the back button is clicked, hide any modals.
+	$(window).on('popstate', function() {
+		$(".modal").modal('hide');
+	});
+
 	// Logs to welcome the player
 	log("Welcome to PokeClicker");
 	log("Click on the pokemon to defeat them!");
